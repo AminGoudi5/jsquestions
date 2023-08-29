9c780d6f055a7abf68e692e4941f277cf1074174
aminGoudi <godarziamin86772@gmail.com>
2023-08-29 08:39:04 +0330

    Function

---
+++ Functions/answers.js
@@ -0,0 +1,85 @@
+function calculate(num1) {
+    return function (num2) {
+        return function (operator) {
+            if (operator === '+') {
+                return num1 + num2;
+            } else if (operator === '-') {
+                return num1 - num2;
+            } else if (operator === '*') {
+                return num1 * num2;
+            } else if (operator === '/') {
+                return num1 / num2;
+            }
+        };
+    };
+}
+
+
+
+function sumFromTo(start, end) {
+    if (start > end) {
+        return 0;
+    }
+
+    return start + sumFromTo(start + 1, end);
+}
+
+
+
+function callOnceAfter(func, seconds) {
+    let lastCalledTime = 0;
+
+    return function (...args) {
+        const currentTime = Date.now();
+        if (currentTime - lastCalledTime >= seconds * 1000) {
+            lastCalledTime = currentTime;
+            return func(...args);
+        }
+    };
+}
+
+
+function limitCallForEveryDuration(func, duration) {
+    let lastCalledTime = 0;
+    const timeThreshold = duration * 1000;
+    return function (...args) {
+        const currentTime = Date.now();
+        if (currentTime - lastCalledTime >= timeThreshold) {
+            lastCalledTime = currentTime;
+            func(...args);
+        }
+    };
+}
+
+
+function rememberOrDo(func) {
+    const results = {};
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (results.hasOwnProperty(key)) {
+            return results[key];
+        } else {
+            const result = func(...args);
+            results[key] = result;
+            return result;
+        }
+    };
+}
+
+
+
+function makeCancelableTimeout(func, time) {
+    let timeoutId;
+  
+    const cancel = () => {
+      if (timeoutId !== undefined) {
+        clearTimeout(timeoutId);
+      }
+    };
+  
+    timeoutId = setTimeout(func, time);
+  
+    return cancel;
+  }
+  
